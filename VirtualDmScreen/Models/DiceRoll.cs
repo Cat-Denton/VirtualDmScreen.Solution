2159525f6267a29b60d36b26adebb47f035fffa0
Cat-Denton <willwdenton@gmail.com>
2021-07-06 20:22:02 -0700

    add Result property and RollDice method to DiceRoll class

--- VirtualDmScreen/Models/DiceRoll.cs
+++ VirtualDmScreen/Models/DiceRoll.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace VirtualDmScreen.Models
 {
@@ -8,7 +9,21 @@ namespace VirtualDmScreen.Models
     public int CharacterId { get; set; }
     public int NumberOfDice { get; set; }
     public int NumberOfSides { get; set; }
+    public int Result { get; set; }
     public DateTime DateTimeStamp { get; set; }
     public virtual Character Character { get; set; }
+
+    public void RollDice() 
+    {
+      var listOfRolls = new List<int>();
+      int result = 0;
+      for (int i = 1; i <= this.NumberOfDice; i++)
+      {
+        Random rnd = new Random();
+        result = result + rnd.Next(1,this.NumberOfSides);
+      } 
+      Console.WriteLine("***********************************************************************************" + result + "**********************************************************************");
+      this.Result = result;
+    }
   }
 }
