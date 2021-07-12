67d26bbc9df68fbf5c735bbda623e05667f78c96
JuanHasbunZem <zemenarwow@gmail.com>
2021-07-12 16:58:04 -0700

    fix HomeController to access user data

--- VirtualDmScreen/Controllers/HomeController.cs
+++ VirtualDmScreen/Controllers/HomeController.cs
@@ -1,21 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Security.Claims;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 using VirtualDmScreen.Models;
 
 namespace VirtualDmScreen.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly VirtualDmScreenContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        public HomeController(UserManager<ApplicationUser> userManager, VirtualDmScreenContext db)
         {
-            _logger = logger;
+        _userManager = userManager;
+        _db = db;
         }
 
         public IActionResult Index()
@@ -24,18 +29,6 @@ namespace VirtualDmScreen.Controllers
         }
 
         public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
-        [Route("/gamepage")]
-        public ActionResult GamePage()
         {
             return View();
         }
